38646b886402a777c69d1b475c36cf86efd6dfe4
nara vate <fvvvate@gmail.com>
2025-10-07 11:34:26 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,10 @@
-def text():
+"""
+this is a meaningless module docstring! i am lying to pylint.
+"""
+
+def text(message):
+    """
+    this function docstring is as meaningless as the module one!
+    i am continuing to lie to pylint.
+    """
     return message
